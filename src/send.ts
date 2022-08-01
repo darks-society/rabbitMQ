e0d32e35c1cbad960d7aaa77ebfbaf0be19abc9d
Anas Ammor <anasmoiammor@gmail.com>
2022-08-01 13:53:47 +0200

    rmq

--- src/send.ts
+++ src/send.ts
@@ -3,26 +3,26 @@
  export function send(){
 
     amqp.connect('amqp://localhost',(err : any,connection : any) => {
-    if(err) {
-        console.log(err)
-    } else {
-        connection.createChannel((err : any,channel : any)=>{
-            if(err){
-                console.log(err)
-            } else {
-                console.log('channel created from sender');
+        if(err) {
+            console.log(err)
+        } else {
+            connection.createChannel((err : any,channel : any)=>{
+                if(err){
+                    console.log(err)
+                } else {
+                    console.log('channel created from sender');
 
-                var queue  : string = 'test'
-                var msg : string = 'first msg'
+                    var queue  : string = 'test'
+                    var msg : string = 'first msg'
 
-                channel.assertQueue(queue,{
-                    durable : false
-                });
+                    channel.assertQueue(queue,{
+                        durable : false
+                    });
 
-                channel.sendToQueue(queue,Buffer.from(msg));
-                console.log("msg sent from sender");
-            }
-        })
-    }
- })
+                    channel.sendToQueue(queue,Buffer.from(msg));
+                    console.log("msg sent from sender");
+                }
+            })
+        }
+    })
  } 
